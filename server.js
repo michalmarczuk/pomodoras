d0071c9862ed62f84bcf44abc5f92edc14a35062
Michał Marczuk <michal.marczuk@globalapptesting.com>
2022-10-31 12:50:43 +0100

    Fix Heroku configuration

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/pomodoras'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/pomodoras/index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/pomodoras/index.html'));
+});
 app.listen(process.env.PORT || 8080);
